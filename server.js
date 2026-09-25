2a63f54ec82134609b95a307b24d016f7b5ba355
edgarVG1 <edgarvgtw@gmail.com>
2026-09-24 19:43:13 -0600

    segundo avance conectar Node.js con MySQL usando la libreria de mysql2

--- server.js
+++ server.js
@@ -2,6 +2,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2');
+
+// Crear la conexión a la base de datos
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345', // Borra esto y pon la contraseña que creaste hace rato
+    database: 'casino_orus'
+});
+
+// Probar la conexión
+conexion.connect((error) => {
+    if (error) {
+        console.error('Error al conectar con MySQL:', error);
+    } else {
+        console.log('¡Conectado exitosamente a la base de datos Casino Orus!');
+    }
+});
 
 // Le decimos a nuestro servidor que la carpeta "public" será accesible para el navegador
 app.use(express.static(path.join(__dirname, 'public')));
